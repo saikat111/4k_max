e38715e586ea36befef246b7216acc2657d7e605
saikat111 <s8856010@gmail.com>
2023-07-12 17:15:22 +0600

    version 8

---
+++ app/src/main/java/com/quantumsoft/fourk/max/flloting_view/minivideomodel.java
@@ -0,0 +1,9 @@
+package com.quantumsoft.fourk.max.flloting_view;
+
+public class minivideomodel {
+
+    public static int mini_video_positipn=0;
+    public static String mini_video_path=null;
+
+
+}
